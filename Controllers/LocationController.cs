200b83f8abc49627ffd6383c14add956acdd5b29
sachinviraj <74185781+Sviraj@users.noreply.github.com>
2024-11-11 23:51:24 +0530

    Add project files.

---
+++ Controllers/LocationController.cs
@@ -0,0 +1,37 @@
+﻿using LocationTracker.Data;
+using LocationTracker.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocationTracker.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public LocationController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveLocation(Location location)
+        {
+            _appDbContext.Locations.Add(location);
+            await _appDbContext.SaveChangesAsync();
+            return Ok(location);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetLocations()
+        {
+            var locations = await _appDbContext.Locations.ToListAsync();
+            return Ok(locations);
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using LocationTracker.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocationTracker.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Location> Locations { get; set; }
+    }
+}
